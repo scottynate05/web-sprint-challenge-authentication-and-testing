769c60fc4c0a0ecb2a637c929232fec9b1811349
Nathan Brown <scottynate05@yahoo.com>
2020-08-14 21:24:34 -0700

    routers and models built, one test suite built

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,88 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const router = require('express').Router();
+const secrets = require('../config/secrets.js');
+const Users = require('../users/users-model.js');
+const { isValid } = require('../users/users-services.js');
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post("/register", (req, res) => {
+  const credentials = req.body;
+
+  if (isValid(credentials)) {
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+
+    // hashing the password
+    const hash = bcrypt.hashSync(credentials.password, rounds);
+    credentials.password = hash;
+
+    // save the user to the database
+    Users.add(credentials)
+      .then((user) => {
+        const token = generateToken(user);
+        res.status(201).json({
+          data: user,
+          jwt_token: token
+        });
+      })
+      .catch((error) => {
+        res.status(500).json({
+          message: error.message
+        });
+      });
+  } else {
+    res.status(400).json({
+      message:
+      'Please provide a username and password (password must be alpha numeric).',
+    });
+  }
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (isValid(req.body)) {
+    Users.findBy({ username: username })
+      .then(([user]) => {
+        // compare the password to the hash in the database.
+        if (user && bcrypt.compareSync(password, user.password)) {
+          const token = generateToken(user);
+          res.status(200).json({
+            message: `Welcome, ${username}!`,
+            jwt_token: token,
+          });
+        } else {
+          res.status(401).json({
+            message: 'You shall not pass!'
+          });
+        }
+      })
+      .catch((error) => {
+        res.status(500).json({
+          message: error.message
+        });
+      });
+  } else {
+    res.status(400).json({
+      message:
+      'Please provide a username and password (password must be alpha numeric).',
+    });
+  };
 });
+
+function generateToken(user) {
+  const payload = {
+      subject: user.id,
+      username: user.username,
+      role: user.role
+  };
+
+  const secret = secrets.jwtSecret;
+  
+  const options = {
+      expiresIn: '1h'
+  };
+
+  return jwt.sign(payload, secret, options);
+};
 
 module.exports = router;
--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,6 @@
+  
+const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@localhost/hobbits";
+
 module.exports = {
   development: {
     client: 'sqlite3',
@@ -8,5 +11,33 @@ module.exports = {
       tableName: 'dbmigrations',
     },
     seeds: { directory: './database/seeds' },
+  },
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/test.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: pgConnection,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
   },
 };
